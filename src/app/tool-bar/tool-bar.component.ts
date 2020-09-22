540fc7ffbe2ac2ded899c19bb33942b9d499ff2d
Erika Grin <evgeny.grin@iprosis.com>
2020-09-23 00:25:13 +0300

    toolbar clean-up and side bar - remove unneeded media query listener, made media-query cleaner

--- src/app/tool-bar/tool-bar.component.ts
+++ src/app/tool-bar/tool-bar.component.ts
@@ -1,30 +1,17 @@
-import { Component, OnInit, Input, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
-
+import { Component } from "@angular/core";
+import { MediaMatcher } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-tool-bar',
-  templateUrl: './tool-bar.component.html',
-  styleUrls: ['./tool-bar.component.css']
+  selector: "app-tool-bar",
+  templateUrl: "./tool-bar.component.html",
+  styleUrls: ["./tool-bar.component.css"],
 })
-export class ToolBarComponent implements OnInit, OnDestroy {
+export class ToolBarComponent {
   mobileQuery: MediaQueryList;
 
-  private _mobileQueryListener: () => void;
-
-  constructor(private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher) { }
+  constructor(private media: MediaMatcher) {}
 
   ngOnInit() {
-    //match media to max-width of 1200
-    this.mobileQuery = this.media.matchMedia('(max-width: 1200px)');
-    //activate litsener to media to be able to listen to media changes
-    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.mobileQuery = this.media.matchMedia("(max-width: 1200px)");
   }
-
-  ngOnDestroy() {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-
-
 }
